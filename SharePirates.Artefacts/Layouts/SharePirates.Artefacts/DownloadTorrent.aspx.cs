ab44750bdb07769414139249a4d514d566e860fb
Harald Fianbakken <harald.fianbakken@gmail.com>
2013-03-08 15:39:37 +0100

    Added sharepirates artefacts project.

---
+++ SharePirates.Artefacts/Layouts/SharePirates.Artefacts/DownloadTorrent.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace SharePirates.Artefacts.Layouts.SharePirates.Artefacts
+{
+    public partial class DownloadTorrent : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
