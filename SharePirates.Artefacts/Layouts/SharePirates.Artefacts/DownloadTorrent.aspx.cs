4c93fcde2b82d0a0376942b682d6cb93b8daeaa8
Harald Fianbakken <harald.fianbakken@gmail.com>
2013-03-08 16:18:25 +0100

    added code to add item to list.
    Added code to add item to list.

--- SharePirates.Artefacts/Layouts/SharePirates.Artefacts/DownloadTorrent.aspx.cs
+++ SharePirates.Artefacts/Layouts/SharePirates.Artefacts/DownloadTorrent.aspx.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
+using SharePirates.Common;
 
 namespace SharePirates.Artefacts.Layouts.SharePirates.Artefacts
 {
@@ -14,9 +15,20 @@ namespace SharePirates.Artefacts.Layouts.SharePirates.Artefacts
 
         protected void AddTorrentToList(Stream stream, string filename)
         {
-//            SPContext.Current.Web.Lists[]
-            
+            var contenttype = SPContext.Current.Web.ContentTypes["TorrentFile"];
+            var lib = SPContext.Current.Web.Lists[SiteDefinition.GetTorrentLib()];
 
+            var data = new byte[stream.Length];
+            stream.Read(data, 0, (int) stream.Length);
+
+            var item = lib.AddItem();
+            item[SPBuiltInFieldId.ContentTypeId] = contenttype.Id;
+            item[SPBuiltInFieldId.Title] = filename;
+            item.Attachments.Add(filename, data);
+            item.SystemUpdate();
+            item.Update();
+            
+            Web.Update();
         }
     }
 }
