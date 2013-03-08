5162ac5f2e033397e7eb92eb6239b3f3935646de
Harald Fianbakken <harald.fianbakken@gmail.com>
2013-03-08 08:02:16 +0100

    Added torrent-parsing framework and html-agility pack

---
+++ SharePirates.Torrent.Parsers/TorrentReactorParser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace SharePirates.Torrent.Parsers
+{
+    public class TorrentReactorParser : IHtmlParser
+    {
+        private readonly WebClient _webClient;
+        private Dictionary<string, string> _values; 
+        private HtmlDocument _document;
+
+        public TorrentReactorParser()
+        {
+               _webClient = new WebClient();
+        }
+
+
+        public Dictionary<string, string> GetMetaData(string url)
+        {
+            LoadHtmlFromUrl(url);
+
+            var node = _document.DocumentNode.SelectSingleNode("//");
+
+
+            return _values;
+        }
+
+        private void LoadHtmlFromUrl(string url)
+        {
+            var html = _webClient.DownloadString(url);
+            _document = new HtmlDocument();
+            _document.LoadHtml(html);
+            _values = new Dictionary<string, string>();
+        }
+    }
+}
