76047058ef8e1cd3c8a07f6adb49bd53bde9b4d3
Per Kristian Helland <perkristianhelland@gmail.com>
2013-03-08 19:01:00 +0100

    Activity monitoring for torrent files

---
+++ SharePirates.TorrentDownloader/ActivityFileMonitor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+namespace SharePirates.TorrentDownloader
+{
+    class ActivityFileMonitor
+    {
+        // System.IO
+        readonly FileSystemWatcher _watchFolder = new FileSystemWatcher();
+        public event EventHandler<FileSystemEventArgs> RaiseFileAddedEvent;
+        public event EventHandler<FileSystemEventArgs> RaiseFileDeletedEvent;
+        public event EventHandler<FileSystemEventArgs> RaiseFileChangedEvent;
+
+        protected virtual void OnRaiseFileChangedEvent(FileSystemEventArgs e)
+        {
+            var handler = RaiseFileChangedEvent;
+            if (handler != null) handler(this, e);
+        }
+
+        protected virtual void OnRaiseFileDeletedEvent(FileSystemEventArgs e)
+        {
+            var handler = RaiseFileDeletedEvent;
+            if (handler != null) handler(this, e);
+        }
+
+        protected virtual void OnRaiseFileAddedEvent(FileSystemEventArgs eventArgs)
+        {
+            var handler = RaiseFileAddedEvent;
+            if (handler != null) {
+                handler(this, eventArgs);
+            }
+        }
+
+        public void StartActivityMonitoring(string sPath)
+        {
+            _watchFolder.Path = sPath;
+            _watchFolder.NotifyFilter = NotifyFilters.DirectoryName;
+            _watchFolder.NotifyFilter =
+            _watchFolder.NotifyFilter | NotifyFilters.FileName;
+            _watchFolder.NotifyFilter =
+            _watchFolder.NotifyFilter | NotifyFilters.Attributes;
+
+            _watchFolder.Changed += EventRaised;
+            _watchFolder.Created += EventRaised;
+            _watchFolder.Deleted += EventRaised;
+
+            try
+            {
+                _watchFolder.EnableRaisingEvents = true;
+            }
+            catch (ArgumentException)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Triggered when an event is raised from the folder activity monitoring.
+        /// All types exists in System.IO
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">containing all data send from the event that got executed.</param>
+        private void EventRaised(object sender, System.IO.FileSystemEventArgs e)
+        {
+            switch (e.ChangeType)
+            {
+                case WatcherChangeTypes.Changed:
+                    OnRaiseFileChangedEvent(e);
+                    break;
+                case WatcherChangeTypes.Created:
+                    OnRaiseFileAddedEvent(e);
+                    break;
+                case WatcherChangeTypes.Deleted:
+                    OnRaiseFileDeletedEvent(e);
+                    break;
+            }
+        } 
+    }
+}
--- SharePirates.TorrentDownloader/Program.cs
+++ SharePirates.TorrentDownloader/Program.cs
@@ -5,7 +5,9 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client.Hubs;
 using Microsoft.SharePoint;
 using SharePirates.Common;
 
@@ -16,53 +18,108 @@ namespace SharePirates.TorrentDownloader
     /// </summary>
     class Program
     {
+        private static IHubProxy _torrentHub;
+        private const string TorrentDownloadPath = @"C:\Users\spdbadmin\Documents\Torrents\TorrentDataDownloading";
+        private const string TorrentAddedPath = @"C:\Users\spdbadmin\Documents\Torrents\TorrentsFinished";
+        private const string TorrentDeletedPath = @"C:\Users\spdbadmin\Documents\Torrents\TorrentsDownloading";
 
-       
         static void Main(string[] args)
         {
+            SetupSignalR();
+            SetupActivityMonitors();
             string url = args.Length < 1 ? SiteDefinition.GetDefaultUrl() : args[0];
-
             using (var site = new SPSite(url))
             {
                 using (var web = site.OpenWeb())
                 {
                     var list = web.Lists[SiteDefinition.GetTorrentLib()];
-
-                    foreach (SPListItem item in list.Items)
+                    while (true)
                     {
-
-                        var downloadFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Torrents"); 
-                        foreach (String attachment in item.Attachments)
+                        foreach (var item in list.Items.Cast<SPListItem>().Where(x => (string)x["FileStatus"] == "Not started" && (string)x["Approved"] == "Approved"))
                         {
-                            var attachmentAbsoluteUrl = item.Attachments.UrlPrefix + attachment;
-                            var file = web.GetFile(attachmentAbsoluteUrl);
-
-                            if (!Directory.Exists(downloadFolder))
+                            const string downloadFolder = TorrentDeletedPath;
+                            foreach (String attachment in item.Attachments)
                             {
-                                Directory.CreateDirectory(downloadFolder);
+                                var attachmentAbsoluteUrl = item.Attachments.UrlPrefix + attachment;
+                                var file = web.GetFile(attachmentAbsoluteUrl);
+                                if (!Directory.Exists(downloadFolder))
+                                {
+                                    Directory.CreateDirectory(downloadFolder);
+                                }
+                                var data = file.OpenBinary();
+                                var torrentName = (string)item["TorrentName"];
+                                var pathTorrentFile = Path.Combine(downloadFolder, torrentName + ".torrent");
+                                File.WriteAllBytes(pathTorrentFile, data);
+                                Console.WriteLine("--File = " + file.Name);
                             }
-                            var data = file.OpenBinary();
-                            var torrentName = (string)item["TorrentName"];
-                            var pathTorrentFile = Path.Combine(downloadFolder, torrentName + ".torrent");
-                            File.WriteAllBytes(pathTorrentFile, data);
-                            try
-                            {
-                                Process.Start(@"C:\Users\spdbadmin\AppData\Roaming\uTorrent\utorrent.exe", pathTorrentFile);
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Ok, so that didn't work quite like we wanted.");
-                            }
-                            
-//                            file.SaveBinary();
-                            Console.WriteLine("--File = "+file.Name);
                         }
+                        Thread.Sleep(60000);
                     }
-
                 }
             }
             Console.WriteLine("Ready..");
             Console.ReadKey();
+        }
+
+        private static void SetupSignalR()
+        {
+            var hubConnection = new HubConnection("http://ciberpirates.apphb.com/");
+            // Create a proxy to the chat service
+            _torrentHub = hubConnection.CreateHubProxy("torrentHub");
+            // Start the connection
+            hubConnection.Start().Wait();
+        }
+
+        private static void SetupActivityMonitors()
+        {
+            var torrentDeletedByUtorrentActivityMonitor = new ActivityFileMonitor();
+            torrentDeletedByUtorrentActivityMonitor.StartActivityMonitoring(TorrentDeletedPath);
+            torrentDeletedByUtorrentActivityMonitor.RaiseFileDeletedEvent += (sender, eventArgs) => FileDeletedByUtorrent(eventArgs);
+            var torrentAddedByUtorrentActivityMonitor = new ActivityFileMonitor();
+            torrentAddedByUtorrentActivityMonitor.StartActivityMonitoring(TorrentAddedPath);
+            torrentAddedByUtorrentActivityMonitor.RaiseFileAddedEvent += (sender, eventArgs) => FileAddedByUtorrent(eventArgs);
+            var fileChangedActivityMonitor = new ActivityFileMonitor();
+            fileChangedActivityMonitor.StartActivityMonitoring(TorrentDownloadPath);
+            fileChangedActivityMonitor.RaiseFileChangedEvent += (sender, eventArgs) => FileChanged(eventArgs);
+        }
+
+        private static void FileChanged(FileSystemEventArgs eventArgs)
+        {
+            var file = Path.GetDirectoryName(eventArgs.FullPath);
+            // TODO: report progress
+        }
+
+        private static void FileAddedByUtorrent(FileSystemEventArgs eventArgs)
+        {
+            var folderName = Path.GetDirectoryName(eventArgs.FullPath);
+            // Torrent is finished
+            // GetListItemByFolderName - foldername == torrentName?
+            _torrentHub.Invoke("TorrentDownloaded", folderName);
+            SetListItemStatus(folderName, "Downloaded");
+        }
+
+        private static void FileDeletedByUtorrent(FileSystemEventArgs eventArgs)
+        {
+            // Torrent is starting downloading
+            var folderName = Path.GetDirectoryName(eventArgs.FullPath);
+            _torrentHub.Invoke("TorrentDownloading", folderName);
+            SetListItemStatus(folderName, "Downloading");
+        }
+
+        private static void SetListItemStatus(string folderName, string fileStatus)
+        {
+            string url = SiteDefinition.GetDefaultUrl();
+            using (var site = new SPSite(url))
+            {
+                using (var web = site.OpenWeb())
+                {
+                    var list = web.Lists[SiteDefinition.GetTorrentLib()];
+                    var finishedTorrentItem = list.Items.Cast<SPListItem>().FirstOrDefault(item => (string) (item["TorrentName"]) == folderName);
+                    if (finishedTorrentItem == null) return;
+                    finishedTorrentItem["FileStatus"] = fileStatus;
+                    finishedTorrentItem.Update();
+                }
+            }
         }
     }
 
