eb7d073689c86d6fd37185bba756ac3cefa5eed3
Per Kristian Helland <perkristianhelland@gmail.com>
2013-03-08 17:17:11 +0100

    Starting utorrent to download torrent files located for each

--- SharePirates.TorrentDownloader/Program.cs
+++ SharePirates.TorrentDownloader/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SharePoint;
@@ -27,12 +30,29 @@ namespace SharePirates.TorrentDownloader
 
                     foreach (SPListItem item in list.Items)
                     {
-                                            
-                        foreach (String attachment in  item.Attachments)
+
+                        var downloadFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Torrents"); 
+                        foreach (String attachment in item.Attachments)
                         {
                             var attachmentAbsoluteUrl = item.Attachments.UrlPrefix + attachment;
                             var file = web.GetFile(attachmentAbsoluteUrl);
 
+                            if (!Directory.Exists(downloadFolder))
+                            {
+                                Directory.CreateDirectory(downloadFolder);
+                            }
+                            var data = file.OpenBinary();
+                            var torrentName = (string)item["TorrentName"];
+                            var pathTorrentFile = Path.Combine(downloadFolder, torrentName + ".torrent");
+                            File.WriteAllBytes(pathTorrentFile, data);
+                            try
+                            {
+                                Process.Start(@"C:\Users\spdbadmin\AppData\Roaming\uTorrent\utorrent.exe", pathTorrentFile);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Ok, so that didn't work quite like we wanted.");
+                            }
                             
 //                            file.SaveBinary();
                             Console.WriteLine("--File = "+file.Name);
